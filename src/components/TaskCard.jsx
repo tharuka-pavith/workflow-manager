ce3192b4f68deb4401353c3272edfd1a8158c0a7
tharuka-pavith <tharukapavith2@gmail.com>
2023-02-25 20:45:19 +0530

    Create TaskCard component

---
+++ src/components/TaskCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from '@mui/material/Card'
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { CardActionArea, CardActions, CardContent } from '@mui/material';
+
+
+function TaskCard(props) {
+    return (
+        <Card elevation={5} sx={{width: "95%"}}>
+            <CardContent>
+                <Typography variant='h6' textAlign='left'>{props.heading}</Typography>
+                <Typography variant='body1' textAlign='left'>Initialized: {props.date}</Typography>
+                <Typography variant='body1' textAlign='left'>{props.details}</Typography>
+            </CardContent>
+            <CardActions>
+            <Button variant='text' size='small'>View More</Button>
+            </CardActions>
+        </Card>
+    );
+}
+
+export default TaskCard;
