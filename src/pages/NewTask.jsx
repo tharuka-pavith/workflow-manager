b19a4ffec9792f4abfb728678f046cfcd1203974
tharuka-pavith <tharukapavith2@gmail.com>
2023-04-24 16:06:14 +0530

    Modify new task

--- src/pages/NewTask.jsx
+++ src/pages/NewTask.jsx
@@ -24,24 +24,24 @@ function mapTypes(task) {
 
 function NewTask(){
     return(
-    <Container maxWidth='lg'>
-        <Paper variant='outlined' sx={{mt: '150px', width: '50%', mx:'auto'}}>
-        <Typography variant='h4' textAlign={'center'} fontWeight="medium" sx={{ my: '10px' }}>New Task</Typography>
+    <Container maxWidth='lg' sx={{mt: '120px', width: '100%'}} disableGutters>
+        {/* <Paper variant='outlined' sx={{mt: '150px', width: '50%', mx:'auto'}}> */}
+        <Typography variant='h5' textAlign={'left'} fontWeight="medium" sx={{ my: '10px' }}>New Task</Typography>
         
-        <Grid container spacing={5} sx={{width: '90%', mx:'auto', my: '20px'}}>
+        <Grid container sx={{mx:'auto', my: '20px'}} spacing={2}>
             <Grid item xs={6}>
             {/* <Typography variant='subtitle1' sx={{display:'inline', my:'auto', height:'75%'}} >Your Task Name: </Typography> */}
-                <TextField fullWidth variant='outlined' label='Task Name'/>
+                <TextField variant='outlined' label='Task Name'/>
             </Grid>
 
-            <Grid item xs={6}>
-                <TextField select fullWidth variant='outlined' label='Task Type' helperText='Please select task type'>
+            {/* <Grid item xs={6}>
+                <TextField select variant='outlined' label='Task Type' helperText='Please select task type'>
                 {taskTypes.map(mapTypes)}
                 </TextField>
-            </Grid>
+            </Grid> */}
 
             <Grid item xs={12}>
-                <TextField fullWidth multiline rows={3} maxRows={3} variant='outlined' label='Description' />
+                <TextField multiline rows={2} maxRows={2} variant='outlined' label='Description' />
             </Grid>
 
             <Grid item xs={12}>
@@ -49,16 +49,17 @@ function NewTask(){
             <Button variant='outlined' startIcon={<Upload/>}>Upload <input hidden accept="image/*" multiple type="file" /></Button>
             </Grid>
             
-            <Grid item xs={4} sx={{width: '75%', mx: 'auto'}}>
-            <Button size={'large'} variant="outlined" color='error'>Cancel</Button>
-            </Grid>
-
-            <Grid item xs={4} sx={{width: '75%', mx: 'auto'}}>
+            <Grid item xs={6} sx={{width: '75%', mx: 'auto'}} >
+            <Button size={'large'} variant="outlined" color='error' sx={{marginRight:'20px'}}>Cancel</Button>
             <Button size={'large'} variant="contained" color='primary'>Submit</Button>
             </Grid>
 
+            <Grid item xs={6} sx={{width: '75%', mx: 'auto'}}>
+            
+            </Grid>
+
         </Grid>
-        </Paper>
+        {/* </Paper> */}
     </Container>
         );
 }
