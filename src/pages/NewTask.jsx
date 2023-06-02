aa7a78aab8fb5589e53b0fa85702cb86cf559be6
tharuka-pavith <tharukapavith2@gmail.com>
2023-06-02 19:09:58 +0530

    Update handleSubmit function

--- src/pages/NewTask.jsx
+++ src/pages/NewTask.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Upload from '@mui/icons-material/Upload';
+import { Paper } from '@mui/material';
 
 //For date picker
 import { LocalizationProvider } from '@mui/x-date-pickers';
@@ -15,7 +16,9 @@ import MenuItem from '@mui/material/MenuItem';
 
 import WorkflowSelect from '../components/WorkFlowSelect';
 
-const currencies = [
+import { useNavigate } from 'react-router-dom';
+
+const taskTypes = [
     {
         value: '1',
         label: 'Cash Voucher',
@@ -35,68 +38,117 @@ const currencies = [
 ];
 
 function NewTask() {
+
+    const navigate = useNavigate();
+
+    const [taskName,setTaskName] = useState('');
+    const [dueDate, setDueDate] = useState(null);
+    const [description, setDescription] = useState('');
+    const [taskType, setTaskType] = useState('');
+    const [workflow, setWorkflow] = useState('');
+
+    const handleWorkflowChange = (values) => {
+        setWorkflow(values);
+      };
+
+    function handleSubmit(event){
+        //todo
+        event.preventDefault();
+        //taskTypes[taskType-1] is not used for now
+        console.log(taskName,description,dueDate);
+        console.log(workflow);
+    }
+
+    function handleDateChange(date){
+        setDueDate(date.$d);
+        console.log(dueDate);
+    }
+
     return (
-        <Container maxWidth='lg' sx={{ mt: '120px', width: '100%' }} disableGutters>
-            {/* <Paper variant='outlined' sx={{mt: '150px', width: '50%', mx:'auto'}}> */}
-            <Typography variant='h5' textAlign={'left'} fontWeight="medium" sx={{ my: '10px' }}>New Task</Typography>
+        <Container maxWidth='lg' sx={{ mt: '120px', width: '100%' }}>
+            <Paper elevation={7} sx={{ padding: '2%' }}>
+                {/* <Paper variant='outlined' sx={{mt: '150px', width: '50%', mx:'auto'}}> */}
+                <Typography variant='h5' textAlign={'left'} fontWeight="medium" sx={{ my: '10px' }}>New Task</Typography>
 
-            <Grid container sx={{ mx: 'auto', my: '10px' }} spacing={4}>
+                <form onSubmit={handleSubmit}>
+                <Grid container sx={{ mx: 'auto', my: '10px' }} spacing={4}>
 
-                <Grid item xs={5}> {/*Right side of the form*/}
-                    <Grid container spacing={4}>
-                        <Grid item xs={12}>
-                            <TextField variant='outlined' label='Task Name' sx={{ width: '70%' }} />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                <DatePicker label='Due Date' />
-                            </LocalizationProvider>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField multiline rows={2} maxRows={2} variant='outlined' label='Description' sx={{ width: '70%' }} />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Typography variant='subtitle1'>Attachments : </Typography>
-                            <Button variant='outlined' startIcon={<Upload />}>Upload <input hidden accept="image/*" multiple type="file" /></Button>
-                        </Grid>
+                    <Grid item xs={5}> {/*Right side of the form*/}
+                        <Grid container spacing={4}>
+                            <Grid item xs={12}>
+                                <TextField variant='outlined' label='Task Name' value={taskName} 
+                                onChange={(e)=>{setTaskName(e.target.value)}} sx={{ width: '70%' }} />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                    <DatePicker value={dueDate} label='Due Date' onChange={handleDateChange} />
+                                </LocalizationProvider>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField multiline rows={2} maxRows={2} variant='outlined' label='Description' 
+                                sx={{ width: '70%' }} helperText="Add some description" value={description}
+                                onChange={(e)=>{setDescription(e.target.value)}}/>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <input
+                                    accept="image/*"
+                                    id="profile-picture-upload"
+                                    type="file"
+                                    
+                                    style={{ display: 'none' }}
+                                />
+                                <Typography variant='subtitle1'>Attachments : </Typography>
+                                <label htmlFor="profile-picture-upload">
+                                <Button variant='outlined' startIcon={<Upload />}>Upload <input hidden accept="image/*" multiple type="file" /></Button>
+                                </label>
+                            </Grid>
 
-                    </Grid>
-                </Grid>
-
-                <Grid item xs={5}> {/*Left Side of the form */}
-                    <Grid container spacing={4}>
-                        <Grid item xs={12}>
-                        <TextField
-                        sx={{width:'60%'}}
-                        id="outlined-select-currency"
-                        select
-                        label="Select"
-                        defaultValue="2"
-                        helperText="Select your task type"
-                    >
-                        {currencies.map((option) => (
-                            <MenuItem key={option.value} value={option.value}>
-                                {option.label}
-                            </MenuItem>
-                        ))}
-                    </TextField>
-                        </Grid>
-
-                        <Grid item xs={12}>
-                        <WorkflowSelect fullWidth />
                         </Grid>
                     </Grid>
-                    
-                    
-                </Grid>
 
-                <Grid item alignContent={'center'} alignItems={'center'} xs={12} sx={{ width: '75%', mx: 'auto' }} >
-                    <Button size={'large'} variant="outlined" color='error' sx={{ marginRight: '20px' }}>Cancel</Button>
-                    <Button size={'large'} variant="contained" color='primary'>Submit</Button>
-                </Grid>
+                    <Grid item xs={5}> {/*Left Side of the form */}
+                        <Grid container spacing={4}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    sx={{ width: '60%' }}
+                                    id="outlined-select-currency"
+                                    select
+                                    label="Task Type"
+                                    defaultValue="2"
+                                    helperText="Select your task type"
+                                    value={taskType}
+                                    onChange={(e)=>{setTaskType(e.target.value)}}
+                                >
+                                    {taskTypes.map((option) => (
+                                        <MenuItem key={option.value} value={option.value}>
+                                            {option.label}
+                                        </MenuItem>
+                                    ))}
+                                </TextField>
+                            </Grid>
 
-            </Grid>
+                            <Grid item xs={12}>
+                                <WorkflowSelect fullWidth 
+                                selectedValues = {workflow}
+                                onChange={handleWorkflowChange} />
+                            </Grid>
+                        </Grid>
+
+
+                    </Grid>
+
+                    <Grid item alignContent={'center'} alignItems={'center'} xs={12} sx={{ width: '75%', mx: 'auto' }} >
+                        <Button size={'large'} variant="outlined" color='error' sx={{ marginRight: '20px' }}
+                        >Cancel</Button>
+                        <Button size={'large'} variant="contained" color='primary' type='submit'
+                        >Submit</Button>
+                    </Grid>
+
+                </Grid>
+                </form>
+            </Paper>
         </Container>
+
     );
 }
 
