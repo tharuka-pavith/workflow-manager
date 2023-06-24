1e47fe5fcd55076e299e22d6731b62e8d5679265
tharuka-pavith <tharukapavith2@gmail.com>
2023-06-24 19:59:42 +0530

    Add custom themes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,42 @@ import React from "react";
 import AppRouter from "./router/AppRouter";
 import FirebaseApp from './firebase/firebaseConfig';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+const theme = createTheme({
+  typography: {
+    button: {
+      textTransform: "none",
+    },
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    fontSize: 14,
+  },
+});
+
 function App() {
   return (
-    <AppRouter />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppRouter />
+    </ThemeProvider>
   );
 }
 
