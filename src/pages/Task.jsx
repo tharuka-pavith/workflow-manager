4e3f480b4d55fa95d34e6111a1a4a7b29e24f6a0
tharuka-pavith <tharukapavith2@gmail.com>
2023-07-22 12:34:58 +0530

    Add functionality to view files of the task (Issue #7)

--- src/pages/Task.jsx
+++ src/pages/Task.jsx
@@ -7,7 +7,8 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 // MUI components
-import { Divider, Button, MenuItem, Typography, Container, Box, Stepper, Step, StepLabel, Grid, Paper } from '@mui/material';
+import { Divider, Button, MenuItem, Typography, Container, Box, Stepper, Step, StepLabel, Grid, Paper, Link, TextField } from '@mui/material';
+
 
 // import Card from '@mui/material/Card';
 // import CardActions from '@mui/material/CardActions';
@@ -16,6 +17,7 @@ import { Divider, Button, MenuItem, Typography, Container, Box, Stepper, Step, S
 // Firebase functions
 import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage, ref, getDownloadURL } from "firebase/storage"; //for storage functionality
 
 // Custom components
 import TaskDialog from '../components/TaskDialog';
@@ -38,6 +40,7 @@ function mapTypes(task) {
 /**Task component */
 function Task(props) {
   const db = getFirestore();
+  const storage = getStorage();
 
   const auth = getAuth();
   const userID = auth.currentUser.uid; //to check whether step is for current user
@@ -48,6 +51,7 @@ function Task(props) {
 
   const [docData, setDocData] = useState({});
   const [workflow, setWorkflow] = useState([]);
+  const [attachments, setAttachments] = useState([]); //Attacments added by task creator Ex. ["file.pdf", "doc.pdf"]
   const [steps, setSteps] = useState([]);
   const [workflowIndex, setWorkflowIndex] = useState(0); //store the workflow array index
   const [rejectedAt, setRejectedAt] = useState(-1); //store the step index if the task is rejected in that step
@@ -79,16 +83,97 @@ function Task(props) {
   };
   /******************* */
 
+  /*
   useEffect(() => {
     const fetchData = async () => {
       const docRef = doc(db, "current_tasks", docID);
       const docSnap = await getDoc(docRef);
-      console.log("doc data",docSnap.data());
+      console.log("doc data", docSnap.data());
       setDocData(docSnap.data());
       setWorkflow(docSnap.data().workflow);
+
+      //get file details and store in attachments
+      const tempArr = docSnap.data().attachments;
+      const filesArr = [];
+      tempArr.forEach((file) => {
+        const fileRef = ref(storage, `tasks/${docID}/${userID}/${file}`);
+        getDownloadURL(fileRef)
+          .then((url) => {
+            console.log("Download url ", url);
+            filesArr.push({ name: file, link: url });
+          })
+          .catch((error) => {
+            // A full list of error codes is available at
+            // https://firebase.google.com/docs/storage/web/handle-errors
+            switch (error.code) {
+              case 'storage/object-not-found':
+                // File doesn't exist
+                break;
+              case 'storage/unauthorized':
+                // User doesn't have permission to access the object
+                break;
+              case 'storage/canceled':
+                // User canceled the upload
+                break;
+  
+              // ...
+  
+              case 'storage/unknown':
+                // Unknown error occurred, inspect the server response
+                break;
+            }
+          });
+         
+      });
+      setAttachments(filesArr);
+      console.log("Attachements ", attachments);
+
+      //setAttachments(docSnap.data().attachments);
     };
+    
     fetchData();
   }, [docID]);
+*/
+  useEffect(() => {
+    const fetchData = async () => {
+      const docRef = doc(db, "current_tasks", docID);
+      const docSnap = await getDoc(docRef);
+      console.log("doc data", docSnap.data());
+      setDocData(docSnap.data());
+      setWorkflow(docSnap.data().workflow);
+
+      //get file details and store in attachments
+      const tempArr = docSnap.data().attachments;
+      const filesArr = [];
+
+      // Create an array of promises using getDownloadURL for each file
+      const downloadURLPromises = tempArr.map((file) => {
+        const fileRef = ref(storage, `tasks/${docID}/${userID}/${file}`);
+        return getDownloadURL(fileRef);
+      });
+
+      // Wait for all promises to resolve
+      try {
+        const downloadURLs = await Promise.all(downloadURLPromises);
+
+        // Map the tempArr and downloadURLs arrays to create the filesArr
+        tempArr.forEach((file, index) => {
+          const url = downloadURLs[index];
+          filesArr.push({ name: file, link: url });
+        });
+
+        setAttachments(filesArr);
+        console.log("Attachments", filesArr);
+      } catch (error) {
+        console.error("Error fetching download URLs", error);
+        // Handle the error if needed
+      }
+    };
+
+    fetchData();
+  }, [docID, userID]);
+
+
 
   useEffect(() => {
     console.log(docData);
@@ -99,22 +184,21 @@ function Task(props) {
     var foundActiveStep = false; //store whether active step found or not
     workflow.forEach((e) => {
       steps.push(e.fullName);
-      if ((!foundActiveStep) && (!e.completed)){ //search for the active step
+      if ((!foundActiveStep) && (!e.completed)) { //search for the active step
         setActiveStep(i);
         foundActiveStep = true;
       }
-      if ((e.completed) && (!e.approved)){ //check for rejected step
+      if ((e.completed) && (!e.approved)) { //check for rejected step
         setRejectedAt(i);
       }
       i++;
     });
-
     console.log(steps);
-
   }, [docData]);
 
+
   const isStepFailed = (step) => {
-    if (rejectedAt !== -1){
+    if (rejectedAt !== -1) {
       return step === rejectedAt;
     }
   };
@@ -127,7 +211,7 @@ function Task(props) {
         <Typography variant='h5' textAlign={'left'} fontWeight="medium" sx={{ my: '10px' }}>Task: {docData.task_name}</Typography>
 
         <TaskDialog open={dialogOpen} handleClose={handleClose}
-          step={selectedStep} isCurrentUser={isCurrentUser} docID={docID} index={workflowIndex} activeStep={activeStep} rejectedAt={rejectedAt}/>
+          step={selectedStep} isCurrentUser={isCurrentUser} docID={docID} index={workflowIndex} activeStep={activeStep} rejectedAt={rejectedAt} />
 
         <Box sx={{ width: '100%', mt: '70px' }}>
           <Stepper activeStep={activeStep} alternativeLabel>
@@ -136,7 +220,7 @@ function Task(props) {
               if (isStepFailed(index)) {
                 labelProps.optional = (
                   <Typography variant="caption" color="error">
-                   Task rejected!
+                    Task rejected!
                   </Typography>
                 );
 
@@ -159,63 +243,40 @@ function Task(props) {
             })}
           </Stepper>
 
-          <Divider sx={{ my: '60px' }} />
+          <Divider sx={{ my: '20px' }} />
 
           {/* <Typography variant='subtitle1' >Task name: {docData.task_name}</Typography>
 
         <Typography variant='subtitle1' >Description: {docData.description}</Typography>
         <Typography variant='subtitle1' >Initialized Date:{docData.initialized_date}</Typography>
-        <Typography variant='subtitle1' >Due Date:{docData.due_date}</Typography> */}
+        <Typography variant='subtitle1' >Due Date:{docData.due_date}</Typography> 
+        docData.task_name, docData.owner_name, docData.initialized_date, docData.due_date
+        docData.description*/}
 
-          <Typography variant='h6' textAlign={'left'} fontWeight="medium" >Attachments: Links</Typography>
-          <Typography variant='h6' textAlign={'left'} fontWeight="medium" sx={{ my: '10px' }}>Details:</Typography>
-          <Grid container>
-            <Grid item xs={4}>
-              <table>
-                <tbody>
-                  <tr>
-                    <td><Typography variant='subtitle1' >Task: </Typography></td>
-                    <td><Typography variant='subtitle1' >{docData.task_name}</Typography></td>
-                  </tr>
-                  <tr>
-                    <td><Typography variant='subtitle1' >Owner: </Typography></td>
-                    <td><Typography variant='subtitle1' >{docData.owner_name}</Typography></td>
-                  </tr>
-                </tbody>
-              </table>
-
-            </Grid>
-
-            <Grid item xs={4}>
-              <table>
-                <tbody>
-                  <tr>
-                    <td><Typography variant='subtitle1' >Initialized Date: </Typography></td>
-                    <td><Typography variant='subtitle1' >{docData.initialized_date}</Typography></td>
-                  </tr>
-                  <tr>
-                    <td><Typography variant='subtitle1' >Due Date: </Typography></td>
-                    <td><Typography variant='subtitle1' >{docData.due_date}</Typography></td>
-                  </tr>
-                  <tr>
-                    <td><Typography variant='subtitle1' >Attachments: </Typography></td>
-                    <td><Typography variant='subtitle1' ><a>Link</a></Typography></td>
-                  </tr>
-                </tbody>
-              </table>
-            </Grid>
-
-            <Grid item xs={4}>
-              <table>
-                <tbody>
-                  <tr>
-                    <td><Typography variant='subtitle1' >Description:{docData.description} </Typography></td>
-                    {/* <td><Typography variant='subtitle1' >{docData.description}</Typography></td> */}
-                  </tr>
-                </tbody>
-              </table>
-            </Grid>
-          </Grid>
+          <Typography variant='h6' textAlign={'left'} fontWeight="medium" >
+            Attachments: {attachments.map(
+              (file) => {
+                return (<Link href={file.link} target='_blank' rel="noopener" underline='hover' sx={{ mx: '1%' }}>
+                  {file.name}</Link>);
+              }
+            )}</Typography>
+          <Typography variant='h6' textAlign={'left'} fontWeight="medium" sx={{ my: '1%' }} >Details:</Typography>
+          <Box sx={{ mx: 'auto',my: '15px'  }}>
+                <Grid container spacing={4} sx={{ my: '1%' }} >
+                    <Grid item xs={3}>
+                        <TextField fullWidth  value={"Owner: " + docData.owner_name} readOnly InputProps={{ readOnly: true, }} variant='standard' />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <TextField fullWidth  value={"Initialized Date: " + docData.initialized_date} InputProps={{ readOnly: true, }} variant='standard' />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <TextField fullWidth  value={"Due Date: " + docData.due_date} InputProps={{ readOnly: true, }} variant='standard' />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField fullWidth  value={docData.description} InputProps={{ readOnly: true, }} variant='standard' />
+                    </Grid>
+                </Grid>
+            </Box>
 
 
           {/* <Typography variant='subtitle1' >Attachments: {docData.attachements}</Typography> */}
