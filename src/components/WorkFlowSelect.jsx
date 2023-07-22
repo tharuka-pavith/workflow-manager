6fd0e93fa023b94c3165bd10688140156bf80ecf
tharuka-pavith <tharukapavith2@gmail.com>
2023-07-22 13:25:00 +0530

    Fix: Remove ability to self-select to the workflow

--- src/components/WorkFlowSelect.jsx
+++ src/components/WorkFlowSelect.jsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 import { useEffect, useState } from 'react';
 
 // MUI components
-import {Autocomplete, TextField, Checkbox} from '@mui/material';
+import { Autocomplete, TextField, Checkbox } from '@mui/material';
 
 //MUI icons
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
@@ -12,17 +12,19 @@ import CheckBoxIcon from '@mui/icons-material/CheckBox';
 
 // Firebase components
 import { collection, getDocs, getFirestore } from 'firebase/firestore';
+import { getAuth } from "firebase/auth";
 
 const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
 const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
 /**WorkflowSelect component */
 export default function WorkflowSelect(props) {
-  const {selectedValues, onChange} = props;
+  const { selectedValues, onChange } = props;
 
   //console.log(selectedValues); // not necessary (Todo: remove)
 
   const db = getFirestore();
+  const auth = getAuth();
 
   const [usersData, setUsersData] = useState([]);
 
@@ -35,30 +37,33 @@ export default function WorkflowSelect(props) {
       try {
         const usersRef = collection(db, 'users');
         const usersSnapshot = await getDocs(usersRef);
-  
+
         const usersDataArray = [];
         usersSnapshot.forEach((doc) => {
           const userData = doc.data();
-          const { user_id, fName, lName } = userData;
-  
-          // Create a map object with user_id as the key and full name as the value
-          const userMap = {
-            user_id,
-            fullName: `${fName} ${lName}`
-          };
-  
-          usersDataArray.push(userMap);
+          const { user_id, fName, lName, full_name } = userData;
+
+          if (user_id !== auth.currentUser.uid) { //if user is not current user, add that user to the array
+            // Create a map object with user_id as the key and full name as the value
+            const userMap = {
+              user_id,
+              fullName: full_name
+            };
+
+            usersDataArray.push(userMap);
+          }
+
         });
-  
+
         setUsersData(usersDataArray);
       } catch (error) {
         console.log('Error fetching users data:', error);
       }
     };
-  
+
     fetchUsersData();
   }, [db]);
-  
+
 
   return (
     <Autocomplete
@@ -82,7 +87,7 @@ export default function WorkflowSelect(props) {
       )}
       style={{ width: 500 }}
       renderInput={(params) => (
-        <TextField {...params} label="Select People" placeholder="Add Assignees" helperText="Select people that you want to add to the workflow and click Add button"/>
+        <TextField {...params} label="Select People" placeholder="Add Assignees" helperText="Select people that you want to add to the workflow and click Add button" />
       )}
     />
   );
